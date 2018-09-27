335ce5ac4d2f62694bfd50f158bdaadbf371c113
leo.messi <605721815@qq.com>
2018-09-27 17:05:13 +0800

    完成主页

--- back/js/common.js
+++ back/js/common.js
@@ -1,4 +1,47 @@
 // 需求: 在发送第一个ajax的时候, 开启进度条, 在全部的ajax回来的时候, 结束进度条
 $(document).ajaxStart(function(){
-  
-})
+  // 开启进度条
+  NProgress.start();
+
+});
+$(document).ajaxStop(function(){
+  // 开启进度条
+  setTimeout(function(){
+    NProgress.done();
+
+  },3000)
+ 
+});
+// 公共的效果
+// 1. 二级菜单切换效果
+// 2. 左侧菜单栏切换
+// 3. 退出功能
+$(function(){
+  $(".nav .category").click(function(){
+    $(".nav .fenlei1").stop().slideToggle();
+  });
+  $(".icon_menu ").click(function(){
+    $('.lt_asode').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+  });
+
+// 3. 点击右侧退出, 显示模态框
+
+$(".icon_logout").click(function(){
+  $("#logoutModal").modal("show");
+});
+$("#logoutbtn").click(function(){
+  // 退出功能, 应该调用后台提供的接口, 在服务器端销毁该用户的登陆状态
+    $.ajax({
+      url:"/employee/employeeLogout",
+      type:"get",
+      dataType:"json",
+      success:function(info){
+        if( info.success ){
+          location.href = "login.html";
+        }
+      }
+    })
+  })
+});
--- back/js/index.js
+++ back/js/index.js
@@ -1,1 +1,98 @@
-$()
+$(function(){
+var echarts_1 = echarts.init(document.querySelector(".echarts_1"));
+
+var option1 = {
+  title: {
+      text: '2017年 注册人数',
+      textStyle: {
+      color: "red",
+
+      }
+  },
+  tooltip: {},
+  legend: {
+      data:['人数']
+  },
+  xAxis: {
+      data: ["1月","2月","3月","4月","5月","6月"]
+  },
+  yAxis: {},
+  series: [{
+      name: '人数',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+  }]
+};
+  // 使用刚指定的配置项和数据显示图表。
+  echarts_1.setOption(option1);
+
+  var echarts_2 = echarts.init(document.querySelector(".echarts_2"));
+
+  // 指定图表的配置项和数据
+  var option2 = {
+    // 大标题
+    title : {
+      text: '热门品牌销售',
+      // 子标题
+      subtext: '2017年6月',
+      // 控制水平对齐方式
+      x:'center'
+    },
+    // 提示框组件
+    tooltip : {
+      trigger: 'item',  // 表示鼠标滑到数据项上面时触发
+
+      // 自定义提示框内容
+      // {a}（系列名称），{b}（数据项名称），{c}（数值）, {d}（百分比）
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+
+    // 图例
+    legend: {
+      // 配置图例的显示方式, horizontal 水平排列
+      orient: 'vertical',
+      // 配置水平对齐方式
+      left: 'left',
+      data: ['耐克','阿迪','李宁','耐克王','李宁王']
+    },
+    series : [
+      {
+        name: '品牌销量',
+        type: 'pie',   // type 类型是 饼状图    line/bar
+        // 圆的大小
+        radius : '50%',
+        // 圆心的位置
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'耐克'},
+          {value:310, name:'阿迪'},
+          {value:234, name:'李宁'},
+          {value:135, name:'耐克王'},
+          {value:1548, name:'李宁王'}
+        ],
+        // 表示额外的阴影等效果
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 50,
+            shadowOffsetX: 0,
+            shadowColor: 'yellow'
+          }
+        }
+      }
+    ]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  echarts_2.setOption(option2);
+
+
+
+
+
+
+
+
+
+
+
+})
